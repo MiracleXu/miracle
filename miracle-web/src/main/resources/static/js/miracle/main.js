3823df424e42742e2e534eed3e4e1cc378c5da9e
xuting <xuting@lvmama.com>
2018-08-22 16:28:34 +0800

    引入vue-resource以及axios插件

--- miracle-web/src/main/resources/static/js/miracle/main.js
+++ miracle-web/src/main/resources/static/js/miracle/main.js
@@ -1,13 +1,24 @@
 new Vue({
     el: '#app',
     data: {
-        dialogVisible: false
+        dialogVisible: false,
+        result:""
     },
     mounted:function() {
-
+        this.init();
     },
     methods: {
-        handleClose(done) {
+        init:function(){
+            var self = this;
+            this.$http.get('http://localhost:8081/test').then(response => {
+                console.log(response.data);
+                self.result = response.data.result;
+                console.log(self.result);
+            }, response => {
+                console.log("error");
+            });
+        },
+        handleClose:function(done) {
             this.$confirm('确认关闭？')
                 .then(_ => {
                     done();
