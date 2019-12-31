3ebb726c95090b19fef989e36e9ab7c29b39f4e1
xuting <xuting@lvmama.com>
2019-12-31 15:18:36 +0800

    增加备案号

--- miracle-web/src/main/resources/static/js/miracle/main.js
+++ miracle-web/src/main/resources/static/js/miracle/main.js
@@ -14,14 +14,14 @@ new Vue({
     methods: {
         init:function(){
             var self = this;
-            this.$http.get('http://www.xuting.xyz/test').then(response => {
-                console.log(response.data);
-                self.result = response.data.result;
-                self.tableData = response.data.firstDao;
-                console.log(self.result);
-            }, response => {
-                console.log("error");
-            });
+            // this.$http.get('http://www.xuting.xyz/test').then(response => {
+            //     console.log(response.data);
+            //     self.result = response.data.result;
+            //     self.tableData = response.data.firstDao;
+            //     console.log(self.result);
+            // }, response => {
+            //     console.log("error");
+            // });
         },
         handleClose:function(done) {
             this.$confirm('确认关闭？')
@@ -29,6 +29,10 @@ new Vue({
                     done();
                 })
                 .catch(_ => {});
+        },
+        open:function () {
+            window.open("http://beian.miit.gov.cn");
+
         }
     }
 })
